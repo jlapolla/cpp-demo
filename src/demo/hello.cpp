9e3edbde4bb559dceac84531c18fcd6f96efde2d
Justin LaPolla <j.a.lapolla@gmail.com>
2017-05-22 16:05:57 +0000

    Add hello.cpp and hello.h

---
+++ src/demo/hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+void hello() {
+
+  std::cout << "Hello World!" << std::endl;
+}
+
--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
+#include "hello.h"
 
 int main() {
 
-  std::cout << "Hello World!";
+  hello();
   return 0;
 }
 
