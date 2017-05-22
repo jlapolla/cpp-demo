7e9ea868b20c7aa3ee5269258e2c2007867057a2
Justin LaPolla <j.a.lapolla@gmail.com>
2017-05-22 20:26:38 +0000

    Add Demo::vector_fixed class

--- src/demo/vector.h
+++ src/demo/vector.h
@@ -1,5 +1,15 @@
 #ifndef DEMO_VECTOR_H
 #define DEMO_VECTOR_H
 
+#include <memory>
+
+namespace Demo {
+
+    template<typename Type, typename Allocator = std::allocator<Type>>
+    class vector_fixed {
+
+    };
+}
+
 #endif // DEMO_VECTOR_H
 
