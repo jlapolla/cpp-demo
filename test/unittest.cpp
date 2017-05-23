81895f96e765049341b96f9cb9eadf35501f8138
Justin LaPolla <j.a.lapolla@gmail.com>
2017-05-23 19:21:21 +0000

    Modify unittest.cpp comments

--- test/unittest.cpp
+++ test/unittest.cpp
@@ -9,21 +9,21 @@ int main() {
     using std::make_unique;
     using std::unique_ptr;
 
-    // Get the top level suite from the registry
+    // Get the top level test suite from the registry
     unique_ptr<CppUnit::Test> suite{CppUnit::TestFactoryRegistry::getRegistry().makeTest()};
 
-    // Adds the test to the list of test to run
+    // Add the test suite to the list of tests to run
     CppUnit::TextUi::TestRunner runner;
     runner.addTest(suite.get());
 
-    // Change the default outputter to a compiler error format outputter
+    // Change the default outputter
     unique_ptr<CppUnit::CompilerOutputter> outputter = make_unique<CppUnit::CompilerOutputter>(&runner.result(), std::cerr);
     runner.setOutputter(outputter.get());
 
-    // Run the tests.
-    bool wasSucessful = runner.run();
+    // Run tests
+    bool success = runner.run();
 
-    // Return error code 1 if the one of test failed.
-    return wasSucessful ? 0 : 1;
+    // Return 1 on failure
+    return success ? 0 : 1;
 }
 
