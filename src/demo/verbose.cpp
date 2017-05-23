c23b091c5bbdcea052e7af3a8acf81d051e86c24
Justin LaPolla <j.a.lapolla@gmail.com>
2017-05-23 17:15:05 +0000

    Use namespace Demo in verbose.cpp

--- src/demo/verbose.cpp
+++ src/demo/verbose.cpp
@@ -1,36 +1,38 @@
 #include "logger.h"
 #include "verbose.h"
 
-Demo::verbose_copy & Demo::verbose_copy::operator=(const Demo::verbose_copy & Right) {
+using namespace Demo;
+
+verbose_copy & verbose_copy::operator=(const verbose_copy & Right) {
 
     global_logger->push_back("verbose_copy & operator=(const verbose_copy & Right)");
     val = Right.val;
     return *this;
 }
 
-Demo::verbose_copy::verbose_copy(Demo::verbose_copy::value_type Val) {
+verbose_copy::verbose_copy(verbose_copy::value_type Val) {
 
     global_logger->push_back("verbose_copy(value_type Val)" );
     val = Val;
 }
 
-Demo::verbose_copy::verbose_copy(const Demo::verbose_copy & Right) {
+verbose_copy::verbose_copy(const verbose_copy & Right) {
 
     global_logger->push_back("verbose_copy(const verbose_copy & Right)" );
     val = Right.val;
 }
 
-Demo::verbose_copy::~verbose_copy() {
+verbose_copy::~verbose_copy() {
 
     global_logger->push_back("~verbose_copy()" );
 }
 
-Demo::verbose_copy::reference Demo::verbose_copy::value() {
+verbose_copy::reference verbose_copy::value() {
 
     return val;
 }
 
-Demo::verbose_copy::const_reference Demo::verbose_copy::value() const {
+verbose_copy::const_reference verbose_copy::value() const {
 
     return val;
 }
