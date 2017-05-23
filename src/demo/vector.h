9fbbd37c506a491f29cbd6afbace2a0b56167fdf
Justin LaPolla <j.a.lapolla@gmail.com>
2017-05-23 11:56:22 +0000

    Add vector_fixed typedefs
    - Still don't have iterator typedefs required by the C++ STL spec.

--- src/demo/vector.h
+++ src/demo/vector.h
@@ -8,6 +8,16 @@ namespace Demo {
     template<typename Type, typename Allocator = std::allocator<Type>>
     class vector_fixed {
 
+        public:
+
+            typedef Allocator allocator_type;
+            typedef typename Allocator::const_pointer const_pointer;
+            typedef typename Allocator::const_reference const_reference;
+            typedef typename Allocator::difference_type difference_type;
+            typedef typename Allocator::pointer pointer;
+            typedef typename Allocator::reference reference;
+            typedef typename Allocator::size_type size_type;
+            typedef typename Allocator::value_type value_type;
     };
 }
 
