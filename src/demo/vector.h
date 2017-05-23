4f8556087a07dbf9204ad5ae4e2d19c1f3205809
Justin LaPolla <j.a.lapolla@gmail.com>
2017-05-23 12:31:33 +0000

    Add minimal vector_fixed member declarations

--- src/demo/vector.h
+++ src/demo/vector.h
@@ -18,6 +18,20 @@ namespace Demo {
             typedef typename Allocator::reference reference;
             typedef typename Allocator::size_type size_type;
             typedef typename Allocator::value_type value_type;
+
+            reference operator[](size_type Pos);
+            const_reference operator[](size_type Pos) const;
+            vector_fixed & operator=(const vector_fixed & right);
+            vector_fixed & operator=(vector_fixed && right);
+
+            reference back();
+            const_reference back() const;
+            size_type capacity() const;
+            bool empty() const;
+            void pop_back();
+            void push_back(const Type & Val);
+            void push_back(Type && Val);
+            size_type size() const;
     };
 }
 
